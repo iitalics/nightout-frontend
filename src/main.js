8a7cabb1b862586d603d4423b2015e5734d5058b
Milo Turner <iitalics@gmail.com>
2015-10-24 15:12:21 -1000

    Initial Commit

---
+++ src/main.js
@@ -0,0 +1,122 @@
+window["allSelected"] = [];
+var searchResults = null;
+
+
+var SearchResult = React.createClass({
+	getInitialState : function () {
+		console.log("init:", this.props.place);
+		return {canAdd : allSelected.indexOf(this.props.place.id) === -1};
+	},
+	render : function () {
+		return (
+			<tr>
+				<td>
+					<div className="row container-fluid">
+						<div className="pull-left">{this.props.place.name}</div>
+						<div className="pull-right">
+							{this.renderButton()}
+						</div>
+					</div>
+				</td>
+			</tr>);
+	},
+	renderButton : function () {
+		if (this.state.canAdd)
+			return (<button className="btn btn-xs btn-success" onClick={this.handleClick}>Add</button>);
+		else
+			return (<button className="btn btn-xs btn-danger" onClick={this.handleClick}>Remove</button>);
+	},
+	handleClick : function () {
+		console.log("click:", this.props.place);
+		var here = this.props.place;
+		if (this.state.canAdd)
+			allSelected.push(here.id);
+		else
+			allSelected = allSelected.filter(function (pl) { return pl.id !== here.id; });
+
+		this.setState({canAdd : !this.state.canAdd})
+	}
+});
+var SearchResults = React.createClass({
+	getInitialState : function () {
+		return {results : []};
+	},
+	render : function () {
+		return (
+			<table className="table table-striped">
+				<tbody>
+				{this.state.results.map(function (data, i) {
+					return (<SearchResult place={data} key={data.id} />)
+				})}
+				</tbody>
+			</table>);
+	}
+});
+var SearchText = React.createClass({
+	render : function () {
+		return (
+			<input type="text" placeholder="Search..." autofocus="yes" className="form-control" onChange={this.handleChange}/>);
+	},
+	handleChange : function (e) {
+		var val = e.target.value;
+		console.log(val);
+
+		performSearch(val, function (r) {
+			searchResults.setState({results : r});
+		});
+	}
+});
+
+function Location (name, address, id) {
+	return { name : name, address : address, id : id };
+}
+
+
+
+var everything = [
+	Location("Eskimo Candy", "", 0),
+	Location("Mama's Fish House", "", 1),
+	Location("Kimo's", "", 2),
+	Location("Da Kitchen Cafe", "", 3),
+	Location("Cafe O'Lei Kihei", "", 4),
+	Location("Sansei Seafood & Sushi Bar", "", 5),
+	Location("Star Noodle", "", 6),
+	Location("Lahaina Grill", "", 7),
+	Location("Paia Fish Market", "", 8),
+	Location("Monkeypod Kitchen by Merriman", "", 9),
+	Location("Mala Ocean Tavern", "", 10),
+	Location("Flatbread Company", "", 11)
+];
+function performSearch (search, cb) {
+	function find (pl) {
+		return pl.name.toLowerCase().indexOf(search.toLowerCase());
+	}
+
+	var r = [];
+	if (search != "") {
+		for (var i = 0; i < everything.length; i++) 
+			if (find(everything[i]) !== -1)
+				r.push(everything[i]);
+
+		r = r.sort(function (a, b) {
+			return find(a) - find(b);
+		});
+	}
+	else
+		r = everything;
+
+	setTimeout(function () {
+		cb(r);
+	}, 100);
+}
+
+
+
+ReactDOM.render(
+	<div className="container">
+		<SearchText />
+		<SearchResults ref={function (self) {
+			searchResults = self;
+		}} />
+	</div>,
+	document.getElementById("everything"));
